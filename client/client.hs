2bcd5e7b9c9209b66db468a75aec2f1e83225e34
juancamilobonet02 <j.bonet@uniandes.edu.co>
2023-03-27 14:05:34 -0500

    avanceswq

--- client/client.hs
+++ client/client.hs
@@ -1,9 +1,42 @@
 module Client where
 
 import Network.Simple.TCP
+import System.Environment
+import Data.Maybe (fromMaybe)
+import Data.Text.Encoding
+import Data.Text ( unpack )
+import           Data.ByteString.Builder
+import qualified Data.ByteString.Lazy               as L
+import qualified Data.ByteString                    as B
 
 main = do
-  connect "192.168.202.135" "8000" $ \(connectionSocket, remoteAddr) -> do
-    putStrLn $ "Connection established to " ++ show remoteAddr
-    -- Now you may use connectionSocket as you please within this scope,
-    -- possibly using recv and send to interact with the remote end.
+  connect "192.168.202.135" "8000" cliente
+
+cliente :: (Socket, SockAddr) -> IO()
+cliente (connectionSocket, remoteAddr) = do
+  putStrLn $ "Connection established to " ++ show remoteAddr
+
+  --Mandar listo
+  sendLazy connectionSocket $ encodeUtf8Txt "ready"
+
+
+  --Recibir num cliente
+  maybeNumCliente <- recv connectionSocket 5
+  let numCliente = maybe "Err" (unpack . decodeUtf8) maybeNumCliente
+
+  --Recibir num clientes totales
+  maybeClientes <- recv connectionSocket 5
+  let clientes = maybe "Err" (unpack . decodeUtf8) maybeClientes
+
+  --Recibir archivo
+  maybeFile <- recv connectionSocket 300000000
+  let fileName = "./ArchivosRecibidos/Cliente"++numCliente++"-Prueba-"++clientes++".txt"
+  let file = fromMaybe B.empty maybeFile
+  B.writeFile fileName file
+
+  putStrLn "Listo"
+
+
+
+encodeUtf8Txt :: String -> L.ByteString
+encodeUtf8Txt = toLazyByteString . stringUtf8
--- server/tcp.hs
+++ server/tcp.hs
@@ -1,10 +1,55 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use when" #-}
 module TCP where
 
-import Network.Simple.TCP
+import           Network.Simple.TCP
+import           Control.Concurrent
+import qualified Data.ByteString.Lazy               as L
+import           Data.ByteString.Builder
+import           Data.Maybe ( isNothing )
+--import           Control.Monad.Loops(untilM_)
 
-main = do 
+main = do
   putStrLn "Starting server..."
-  serve (Host "0.0.0.0") "8000" $ \(connectionSocket, remoteAddr) -> do
-    putStrLn $ "TCP connection established from " ++ show remoteAddr
+  putStrLn "Cual archivo quiere mandar?"
+  file <- readLn
+  putStrLn "Cuantos clientes quieres atender a la vez"
+  clientes <- readLn
+  clientesConn <- newMVar 0
+  serve (Host "0.0.0.0") "8000" (handleSocket clientesConn clientes file)
   -- Now you may use connectionSocket as you please within this scope,
-  -- possibly using recv and send to interact with the remote end.
+  -- possibly using recv and send to interact with the remote end.kkkk
+
+handleSocket :: MVar Integer -> Integer -> String -> (Socket, SockAddr) -> IO()
+handleSocket totalMvar clientes file (connectionSocket,remoteAddr) = do
+  putStrLn $ "TCP connection established from " ++ show remoteAddr
+  maybeReady <- recv connectionSocket 10
+  -- TODO exception
+  putStrLn $ "Cliente listo: " ++ show remoteAddr
+  clientNum <- takeMVar totalMvar
+  putMVar totalMvar (clientNum + 1)
+  untilMVar (== clientes) totalMvar
+
+  --Mandar numero cliente
+  sendLazy connectionSocket $ encodeUtf8Txt $ show clientNum
+
+  --Mandar numero total clientes
+  sendLazy connectionSocket $ encodeUtf8Txt $ show clientes
+
+  -- Mandar archivo
+  contenido <- L.readFile file
+  sendLazy connectionSocket contenido
+  putStrLn $ "Cliente atendido: " ++ show remoteAddr
+
+  --TODO hash y logs
+
+
+untilMVar :: (a -> Bool) -> MVar a -> IO()
+untilMVar p mvar = do
+  x <- readMVar mvar
+  if not (p x)
+    then untilMVar p mvar
+    else return ()
+
+encodeUtf8Txt :: String -> L.ByteString
+encodeUtf8Txt = toLazyByteString . stringUtf8
